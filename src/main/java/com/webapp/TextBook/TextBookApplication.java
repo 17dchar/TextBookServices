e3e8ba06e50310768669557c963936accd17cc4b
Zachariah Durbin <42813945+ZachariahDurbin@users.noreply.github.com>
2021-03-18 05:45:14 -0500

    Finished Database Testing

--- src/main/java/com/webapp/TextBook/TextBookApplication.java
+++ src/main/java/com/webapp/TextBook/TextBookApplication.java
@@ -30,6 +30,14 @@ public class TextBookApplication implements CommandLineRunner {
 
 		List<SCBCRSEModel> scbcrseList = jdbcTemplate.query(sql,
 				BeanPropertyRowMapper.newInstance(SCBCRSEModel.class));
+
+		scbcrseList.forEach(System.out :: println);
+		String sqlChanging = "UPDATE SCBCRSE;" +
+				"SET SCBCRSE_CRSE_NUMB = 'haha'";
+
+		scbcrseList = jdbcTemplate.query(sqlChanging,
+				BeanPropertyRowMapper.newInstance(SCBCRSEModel.class));
+
 		scbcrseList.forEach(System.out :: println);
 	}
 }
