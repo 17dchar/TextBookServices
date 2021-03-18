6d1c7971da5f44317fc21d26b74e8eb5a272a622
Zachariah Durbin <42813945+ZachariahDurbin@users.noreply.github.com>
2021-03-18 05:24:06 -0500

    Connection to Database Finished

--- src/main/java/com/webapp/TextBook/TextBookApplication.java
+++ src/main/java/com/webapp/TextBook/TextBookApplication.java
@@ -1,22 +1,35 @@
 package com.webapp.TextBook;
 
+import com.webapp.TextBook.Model.SCBCRSEModel;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.JdbcTemplateAutoConfiguration;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import java.util.List;
 
 @SpringBootApplication
-public class TextBookApplication {
+public class TextBookApplication implements CommandLineRunner {
+
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
 
 	public static void main(String[] args) {
+		System.out.println("I work!!!");
 		SpringApplication.run(TextBookApplication.class, args);
 	}
 
-	/*@Bean
-	public DataSource getDataSource(){
-		return DataSourceBuilder.create()
-				.url("jdbc:oracle:thin@localhost:1521/orcl")
-				.username("textbookservices")
-				.password("Textbookpassword2021")
-				.build();
-	}*/
 
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("I am now in the database part!");
+		String sql = "SELECT * FROM SCBCRSE";
+
+		List<SCBCRSEModel> scbcrseList = jdbcTemplate.query(sql,
+				BeanPropertyRowMapper.newInstance(SCBCRSEModel.class));
+		scbcrseList.forEach(System.out :: println);
+	}
 }
