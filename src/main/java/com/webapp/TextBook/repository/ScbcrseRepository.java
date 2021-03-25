f99de032f2254f1243febb52179e6035297dd0e2
Zachariah Durbin <42813945+ZachariahDurbin@users.noreply.github.com>
2021-03-25 13:50:05 -0500

    Basics of Scbcrse Repository Done

--- src/main/java/com/webapp/TextBook/repository/ScbcrseRepository.java
+++ src/main/java/com/webapp/TextBook/repository/ScbcrseRepository.java
@@ -1,13 +1,13 @@
 package com.webapp.TextBook.repository;
 
-import com.webapp.TextBook.Model.Spriden;
+import com.webapp.TextBook.Model.Scbcrse;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-public interface SpridenRepository extends JpaRepository<Spriden, Integer>{
+public interface ScbcrseRepository extends JpaRepository<Scbrse, Integer>{
     @Override
-    List<Scbcrse> findAll();
+    List<Spriden> findAll();
 }
