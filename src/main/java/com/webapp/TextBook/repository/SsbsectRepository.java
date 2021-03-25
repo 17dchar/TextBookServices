69bb95b126c1099a03d97c15fc0de713f45c7167
Zachariah Durbin <42813945+ZachariahDurbin@users.noreply.github.com>
2021-03-25 14:03:07 -0500

    Basics of Ssbsect Repository Done

--- src/main/java/com/webapp/TextBook/repository/SsbsectRepository.java
+++ src/main/java/com/webapp/TextBook/repository/SsbsectRepository.java
@@ -1,13 +1,14 @@
 package com.webapp.TextBook.repository;
 
-import com.webapp.TextBook.Model.Sfrstcr;
+import com.webapp.TextBook.Model.Ssbsect;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-public interface SfrstcrRepository extends JpaRepository<Sfrstcer, Integer>{
+public interface SsbsectRepository extends JpaRepository<Ssbsect, Integer>{
     @Override
-    List<Sfrstcr> findAll();
+    List<Ssbsect> findAll();
 }
+
