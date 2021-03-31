f5f648afc92d6736dde715cf5e756033c7f1b232
Zachariah Durbin <42813945+ZachariahDurbin@users.noreply.github.com>
2021-03-31 13:58:33 -0500

    Adding Mask Configuration!

---
+++ src/main/java/com/webapp/TextBook/Configuration/MaskConfig.java
@@ -0,0 +1,59 @@
+package com.webapp.TextBook.Configuration;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+
+@Configuration
+@EnableWebMvc
+public class MaskConfig implements WebMvcConfigurer {
+    @Autowired
+    private ApplicationContext applicationContext;
+    private static final String[] CLASSPATH_RESOURCE_LOCATIONS = {
+            "classpath:/META-INF/resources/", "classpath:/resources/",
+            "classpath:/static/", "classpath:/public/"
+    };
+
+    @Bean
+    public ViewResolver viewResolver(){
+        InternalResourceViewResolver bean = new InternalResourceViewResolver();
+        bean.setPrefix("/WEB-INF/jsp/");
+        bean.setSuffix(".jsp");
+        bean.setOrder(0); //set to 1 if you want to use thymeleaf as initial view in controller
+        return bean;
+    }
+    @Bean
+    public WebMvcConfigurer forwardToIndex() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addViewControllers(ViewControllerRegistry registry) {
+                registry.addViewController("/").setViewName(
+                        "forward:/WEB-INF/jsp/login.jsp");
+                registry.addViewController("/bookQuery").setViewName(
+                        "forward:/WEB-INF/jsp/bookQuery.jsp");
+                registry.addViewController("/addBook").setViewName(
+                        "forward:/WEB-INF/jsp/addBook.jsp");
+
+            }
+        };
+
+
+
+
+    }
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**")
+                .addResourceLocations(CLASSPATH_RESOURCE_LOCATIONS);
+
+    }
+}
