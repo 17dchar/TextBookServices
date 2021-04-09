9221bdbd708b0c1198b23812618d24b844dbe643
Zachariah Durbin <42813945+ZachariahDurbin@users.noreply.github.com>
2021-04-09 01:42:34 -0500

    Supervisor Privileges Finished

--- src/main/java/com/webapp/TextBook/controller/HomeController.java
+++ src/main/java/com/webapp/TextBook/controller/HomeController.java
@@ -74,17 +74,26 @@ public class HomeController {
     @Autowired
     AddBookService addBookService;
 
+    //Add-Book GET
+    //SUPERVISOR ACCESS ONLY
     @GetMapping("/Add-Book")
     public String addBookPage(){
         System.out.println("Add Book GET");
+        if(!supervisor){
+            login();
+        }
         return "Supervisor/addBook";
     }
 
+    //Add-Book POST
+    //SUPERVISOR ACCESS ONLY
     @PostMapping("/Add-Book")
     public String addBookPOST(@Valid Nwtxdt nwtxdt, BindingResult bindingResult,
                               ModelMap model) throws ParseException {
         System.out.println("Add Book POST");
-
+        if(!supervisor){
+            login();
+        }
         //If There Are Errors Compared To The Model, Then It Will Return Invalid Credentials
         if(bindingResult.hasErrors()){
             model.put("returnVoidError", "Invalid Credentials");
@@ -105,18 +114,28 @@ public class HomeController {
     @Autowired
     BookQueryService bookQueryService;
 
+    //Find-Book GET
+    //SUPERVISOR ONLY
     @GetMapping("/Find-Book")
     public String bookQuery(ModelMap model){
         System.out.println("Book Query GET");
+        if(!supervisor){
+            login();
+        }
         return "Supervisor/bookQuery";
     }
 
+    //Find-Book POST
+    //SUPERVISOR ONLY
     @PostMapping("/Find-Book")
     public String bookQueryPost(@Valid Nwtxdt nwtxdt, BindingResult bindingResultNwtxdt,
                                 @Valid Nwtxin nwtxin, BindingResult bindingResultNwtxin,
                                 ModelMap model)
                                 throws ParseException {
         System.out.println("Book Query POST");
+        if(!supervisor){
+            login();
+        }
         if(bindingResultNwtxdt.hasErrors() || bindingResultNwtxin.hasErrors()){
             model.put("returnVoidError", "Invalid Credentials");
             for (Object object : bindingResultNwtxdt.getAllErrors()) {
@@ -157,17 +176,27 @@ public class HomeController {
     @Autowired
     BookDispositionService bookDispositionService;
 
+    //Change Disposition GET
+    //SUPERVISOR ONLY
     @GetMapping("/Change-Disposition")
     public String bookDisposition(){
         System.out.println("Book Dispostion GET");
+        if(!supervisor){
+            login();
+        }
         return "Supervisor/bookDisposition";
     }
 
-    @PostMapping("/Disposition-Change")
+    //Change Disposition POST
+    //SUPERVISOR ONLY
+    @PostMapping("/Change-Disposition")
     public String bookDispositionPost(@Valid Nwtxdt nwtxdt, BindingResult bindingResult,
                                       ModelMap model)
                                       throws ParseException {
         System.out.println("Book Disposition POST");
+        if(!supervisor){
+            login();
+        }
         if(bindingResult.hasErrors()){
             model.put("returnVoidError", "Invalid Credentials");
             for (Object object : bindingResult.getAllErrors()) {
@@ -187,17 +216,28 @@ public class HomeController {
 
     @Autowired
     ReplaceBarcodeService replaceBarcodeService;
+
+    //Replace Barcode GET
+    //SUPERVISOR ONLY
     @RequestMapping(value = "/Change-Barcode", method = RequestMethod.GET)
     public String replaceBarcode() {
         System.out.println("Replace Barcode GET");
+        if(!supervisor){
+            login();
+        }
         return "Supervisor/replaceBarcode";
     }
 
+    //Replace Barcode POST
+    //SUPERVISOR ONLY
     @RequestMapping(value = "/replaceBarcode", method = RequestMethod.POST)
     public String replaceBarcodePOST(@Valid Nwtxdt nwtxdt, BindingResult bindingResult,
                                      ModelMap model)
                                      throws ParseException {
         System.out.println("Replace Barcode POST");
+        if(!supervisor){
+            login();
+        }
         if(bindingResult.hasErrors()){
             model.put("returnVoidError", "Invalid Credentials");
             for (Object object : bindingResult.getAllErrors()) {
@@ -238,15 +278,27 @@ public class HomeController {
 
     @Autowired
     QueryCourseService queryCourseService;
+
+    //Find Course GET
+    //SUPERVISOR ONLY
     @GetMapping("/Find-Course")
     public String queryCourse(){
         System.out.println("Course Query GET");
+        if(!supervisor){
+            login();
+        }
         return "Supervisor/queryCourse";
     }
+
+    //Find Course POST
+    //SUPERVISOR ONLY
     @PostMapping("/Find-Course")
     public String queryCoursePost(ModelMap model, Scbcrse scbcrse,BindingResult bindingResult)
                                   throws ParseException {
         System.out.println("Course Query POST");
+        if(!supervisor){
+            login();
+        }
         //Pseudo Regex
         if(bindingResult.hasErrors()){
             model.put("returnVoidError", "Invalid Credentials");
@@ -271,16 +323,27 @@ public class HomeController {
 
     @Autowired
     CourseMessageService courseMessageService;
+
+    //Course Message GET
+    //SUPERVISOR ONLY
     @RequestMapping(value = "/Course-Message", method = RequestMethod.GET)
     public String courseMessage(){
         System.out.println("Course Message GET");
+        if(!supervisor){
+            login();
+        }
         return "Supervisor/courseMessage";
     }
 
+    //Course Message POST
+    //SUPERVISOR ONLY
     @RequestMapping(value = "/Course-Message", method = RequestMethod.POST)
     public String courseMessagePost(ModelMap model, Nwtxcm nwtxcm, BindingResult bindingResult)
                                     throws ParseException{
         System.out.println("Course Message POST");
+        if(!supervisor){
+            login();
+        }
         //Pseudo Regex
         if(bindingResult.hasErrors()){
             model.put("returnVoidError", "Invalid Credentials");
@@ -289,7 +352,7 @@ public class HomeController {
                     System.out.println((FieldError) object);
                 }
             }
-            return "Supervisor/queryCourse";
+            return "Supervisor/courseMessage";
         }
 
         Nwtxcm oldNwtxcm = courseMessageService.getNwtxcm(nwtxcm.getCmCourse());
@@ -301,10 +364,16 @@ public class HomeController {
         }
         return "Supervisor/courseMessage";
     }
+
+    //Course Message CLEAR
+    //SUPERVISOR ONLY
     @RequestMapping(value = "/Course-Message", method = RequestMethod.POST, params="clear")
     public String courseMessageClear(ModelMap model, Nwtxcm nwtxcm, BindingResult bindingResult)
                                      throws ParseException {
         System.out.println("Course Message POST - CLEAR");
+        if(!supervisor){
+            login();
+        }
         //Pseudo Regex
         if(bindingResult.hasErrors()){
             model.put("returnVoidError", "Invalid Credentials");
@@ -313,7 +382,7 @@ public class HomeController {
                     System.out.println((FieldError) object);
                 }
             }
-            return "Supervisor/queryCourse";
+            return "Supervisor/courseMessage";
         }
 
         Nwtxcm oldNwtxcm = courseMessageService.getNwtxcm(nwtxcm.getCmCourse());
@@ -330,16 +399,27 @@ public class HomeController {
 
     @Autowired
     ChangeBookcodeService changeBookcodeService;
+
+    //Change Book Code GET
+    //SUPERVISOR ONLY
     @RequestMapping(value= "/Change-Book-Code", method = RequestMethod.GET)
     public String changeBookCode(){
         System.out.println("Course Message GET");
+        if(!supervisor){
+            login();
+        }
         return "Supervisor/changeBookCode";
     }
 
+    //Change Book Code POST
+    //SUPERVISOR ONLY
     @RequestMapping(value= "/Book-Code-Change", method = RequestMethod.POST)
     public String changeBookCodePost(ModelMap model, Nwtxin nwtxin, BindingResult bindingResult)
                                      throws ParseException{
         System.out.println("Course Message POST");
+        if(!supervisor){
+            login();
+        }
         //Pseudo Regex
         if(bindingResult.hasErrors()){
             model.put("returnVoidError", "Invalid Credentials");
@@ -358,7 +438,7 @@ public class HomeController {
             //oldNwtxin.setBookCode(newBookCode); //<-- This is the changed Item
             //oldNwtxin.setEditionYear(newEditionYear); //<-- This is the changed Item
 
-            changeBookcodeService.deleteNwtxin(bookCode);
+            //changeBookcodeService.deleteNwtxin(bookCode);
             changeBookcodeService.saveNwtxin(oldNwtxin);
         }
         return "Supervisor/changeBookCode";
@@ -367,12 +447,22 @@ public class HomeController {
 
     @Autowired
     PreviousBooksService previousBooksService;
+
+    //Previous Books GET
     @RequestMapping(value= "/Previous-Books", method = RequestMethod.GET)
     public String prevBoooks(){
         System.out.println("Previous Books GET");
-        return "StudentEmployee/previousBooks";
+        if(supervisor){
+            return"Supervisor/previousBooks";
+        } else if(studentEmployee){
+            return "StudentEmployee/previousBooks";
+        } else {
+            login();
+        }
+        return "login";
     }
 
+    //Previoius Books POST
     @RequestMapping(value= "/Previous-Books", method = RequestMethod.POST)
     public String prevBooksPost(ModelMap model,
                                 @RequestParam(value = "id", required = false, defaultValue = "")String id,
@@ -382,6 +472,10 @@ public class HomeController {
         //Pseudo Regex
         if(id.equals("") || prevTerm.equals("")){
             System.out.println("nah dawg");
+            //Supervisor Privileges
+            if(supervisor){
+                return"Supervisor/previousBooks";
+            }
             return "StudentEmployee/previousBooks";
         }
 
@@ -390,25 +484,42 @@ public class HomeController {
         if(spriden != null && stvterm != null){
             model.put("prevBooks",previousBooksService.getNwtxdt(spriden.getPidm(), prevTerm));
         }
-
+        //Supervisor Privileges
+        if(supervisor){
+            return"Supervisor/previousBooks";
+        }
         return "StudentEmployee/previousBooks";
     }
 
 
     @Autowired
     SoldBooksService soldBooksService;
+
+    //Sold Books GET
     @RequestMapping(value= "/Sold-Books", method = RequestMethod.GET)
     public String soldBooks(){
         System.out.println("Sold Books GET");
-        return "StudentEmployee/soldBooks";
+        if(supervisor){
+            return"Supervisor/soldBooks";
+        } else if(studentEmployee){
+            return "StudentEmployee/soldBooks";
+        } else {
+            login();
+        }
+        return "login";
     }
 
+    //Sold Books POST
     @RequestMapping(value= "/Sold-Books", method = RequestMethod.POST)
     public String soldBooksPost(ModelMap model,
                                 @RequestParam(value = "id", required = false, defaultValue = "")String id){
         System.out.println("Sold Books Post");
         //Pseudo Regex
         if(id.equals("")){
+            //Supervisor Privileges
+            if(supervisor){
+                return"Supervisor/soldBooks";
+            }
             return "StudentEmployee/soldBooks";
         }
 
@@ -416,34 +527,58 @@ public class HomeController {
         if(spriden != null){
             model.put("soldBooks",soldBooksService.getNwtxdt(spriden.getPidm()));
         }
-
+        //Supervisor Privileges
+        if(supervisor){
+            return"Supervisor/soldBooks";
+        }
         return "StudentEmployee/soldBooks";
     }
 
 
     @Autowired
     StudentScheduleService studentScheduleService;
+
+    //Student Schedule GET
     @RequestMapping(value= "/Student-Schedule", method = RequestMethod.GET)
     public String studentSchedule(){
         System.out.println("Student Schedule GET");
-        return "StudentEmployee/studentSchedule";
+        if(supervisor){
+            return"Supervisor/studentSchedule";
+        } else if(studentEmployee){
+            return "StudentEmployee/studentSchedule";
+        } else {
+            login();
+        }
+        return "login";
     }
 
+    //Student Schedule POST
     @RequestMapping(value= "/Student-Schedule", method = RequestMethod.POST)
     public String studentSchedulePost(ModelMap model,
                                       @RequestParam(value = "termSeason", required = false, defaultValue = "")String termSeason,
                                       @RequestParam(value = "id", required = false, defaultValue = "")String id)
                                       throws ParseException{
         System.out.println("Student Schedule POST");
+        if(!supervisor && !studentEmployee){
+            return"login";
+        }
         //Pseudo Regex
         if(termSeason.equals("") || id.equals("")){
             System.out.println("nah dawg");
+            //Supervisor Privileges
+            if(supervisor){
+                return"Supervisor/studentSchedule";
+            }
             return "StudentEmployee/studentSchedule";
         }
 
         studentScheduleService.getStvterm(termSeason).getDesc();
         if(studentScheduleService.getSpriden(id).getPidm().equals("")){
             System.out.println("pidm empty");
+            //Supervisor Privileges
+            if(supervisor){
+                return"Supervisor/studentSchedule";
+            }
             return "StudentEmployee/studentSchedule";
         }
         int pidm = Integer.parseInt(studentScheduleService.getSpriden(id).getPidm());
@@ -465,9 +600,10 @@ public class HomeController {
         model.put("output", output);
         model.put("outputTitle", outputTitle);
         model.put("outputTimes", outputTimes);
-
-
-        return "StudentEmployee/studentSchedule";
+        //Supervisor Privileges
+        if(supervisor){
+            return"Supervisor/studentSchedule";
+        }
+            return "StudentEmployee/studentSchedule";
     }
-
 }
